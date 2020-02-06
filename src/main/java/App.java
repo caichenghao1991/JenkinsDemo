61cd6ec43a21ffaea35e9bc6fedc1bff0a983432
LAPTOP-6GCRBPGJ\caich <ccai@udel.edu>
2020-02-06 16:00:52 -0600

    test fail

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,7 @@
-public class app {
+public class App {
     public static void main(String[] args){
         System.out.println("Hello World");
         Calculator calculator=new Calculator();
-        System.out.println(calculator.addition(1,2));
+        System.out.println(calculator.addition(1));
     }
 }
