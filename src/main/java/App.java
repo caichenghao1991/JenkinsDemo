9b0603eea8e87c05341b77bfdce23f3f0e908006
caichenghao1991 <32524226+caichenghao1991@users.noreply.github.com>
2020-02-06 16:10:58 -0600

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args){
         System.out.println("Hello World");
         Calculator calculator=new Calculator();
-        System.out.println(calculator.addition(11));
+        System.out.println(calculator.addition(1，2));
     }
 }
